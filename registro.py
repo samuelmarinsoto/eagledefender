860e60a1421cca117a243c27c8794a24e461ed70
joscCE <j.campos@estudiantec.cr>
2023-10-02 17:56:45 -0600

    interfaz 65%

--- registro.py
+++ registro.py
@@ -3,6 +3,11 @@ import tkinter.messagebox
 import customtkinter
 import tkinter.filedialog as filedialog
 from PIL import Image, ImageTk
+import os
+import cv2
+from matplotlib import pyplot
+from mtcnn.mtcnn import MTCNN
+import numpy as np
 
 
 # customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
@@ -92,9 +97,12 @@ class Registro(customtkinter.CTk):
         self.entry_Contra = customtkinter.CTkEntry(self.tabview.tab("Juego"), show="◊")
         self.entry_Contra.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
 
+        self.foto_label = customtkinter.CTkLabel(self.tabview.tab("Juego"), corner_radius=60, text="Foto")
+        self.foto_label.place(relx=0.5, rely=0.7, anchor=customtkinter.CENTER)
 
-        self.foto = customtkinter.CTkFrame(self.tabview.tab("Juego"), fg_color=grey, corner_radius=100, height=80,width=80)
-        self.foto.place(relx=0.5, rely=0.7, anchor=customtkinter.CENTER)
+
+        # self.foto = customtkinter.CTkFrame(self.tabview.tab("Juego"), fg_color=grey, corner_radius=100, height=80,width=80)
+        # self.foto.place(relx=0.5, rely=0.7, anchor=customtkinter.CENTER)
 
         self.subir_Foto = customtkinter.CTkButton(self.tabview.tab("Juego"), text="✚",
                                                         fg_color=green_light, hover_color=green, corner_radius=80, width=10, command=self.abrir_archivo)
@@ -124,7 +132,7 @@ class Registro(customtkinter.CTk):
         self.cancion3.place(relx=0.5, rely=0.7, anchor=customtkinter.CENTER)
 
         self.sidebar_button_1 = customtkinter.CTkButton(self.tabview.tab("Personalizacion"), text="Registrarse",
-                                                    fg_color=green_light, hover_color=green)
+                                                    fg_color=green_light, hover_color=green, command=self.registro_facial)
         self.sidebar_button_1.place(relx=0.5, rely=0.9, anchor=customtkinter.CENTER)
 
 
@@ -141,10 +149,10 @@ class Registro(customtkinter.CTk):
             imagen = imagen.resize((80, 80), Image.ANTIALIAS)
             # Convertir la imagen en un formato compatible con Tkinter
             imagen_tk = ImageTk.PhotoImage(imagen)
-            # Mostrar la imagen en el CTkFrame
-            self.foto.configure(image=imagen_tk)
-            self.foto.image = imagen_tk  # ¡Importante! Debes mantener una referencia a la imagen para que no se elimine de la memoria
-
+            # Mostrar la imagen en el CTkLabel
+            self.foto_label.configure(image=imagen_tk)
+            self.foto_label.configure(text="")
+            self.foto_label.image = imagen_tk  # ¡Importante! Debes mantener una referencia a la imagen para que no se elimine de la memoria
 
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
@@ -153,3 +161,40 @@ class Registro(customtkinter.CTk):
     def change_scaling_event(self, new_scaling: str):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
+
+    def registro_facial(self):
+            # Vamos a capturar el rostro
+        cap = cv2.VideoCapture(0)  # Elegimos la camara con la que vamos a hacer la deteccion
+        while (True):
+            ret, frame = cap.read()  # Leemos el video
+            frame = np.flip(frame, axis=1)
+            cv2.imshow('Registro Facial', frame)  # Mostramos el video en pantalla
+            if cv2.waitKey(1) == 27:  # Cuando oprimamos "Escape" rompe el video
+                break
+        usuario_img = self.entry_Username.get()
+        cv2.imwrite(usuario_img + ".jpg",
+                        frame)  # Guardamos la ultima caputra del video como imagen y asignamos el nombre del usuario
+        cap.release()  # Cerramos
+        cv2.destroyAllWindows()
+
+
+
+        def reg_rostro(img, lista_resultados):
+            data = pyplot.imread(img)
+            for i in range(len(lista_resultados)):
+                x1, y1, ancho, alto = lista_resultados[i]['box']
+                x2, y2 = x1 + ancho, y1 + alto
+                pyplot.subplot(1, len(lista_resultados), i + 1)
+                pyplot.axis('off')
+                cara_reg = data[y1:y2, x1:x2]
+                cara_reg = cv2.resize(cara_reg, (150, 200),
+                                        interpolation=cv2.INTER_CUBIC)  # Guardamos la imagen con un tamaño de 150x200
+                cv2.imwrite(usuario_img + ".jpg", cara_reg)
+                pyplot.imshow(data[y1:y2, x1:x2])
+            pyplot.show()
+
+        img = usuario_img + ".jpg"
+        pixeles = pyplot.imread(img)
+        detector = MTCNN()
+        caras = detector.detect_faces(pixeles)
+        reg_rostro(img, caras)
