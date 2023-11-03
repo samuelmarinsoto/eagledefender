4c7a9c4ddf2768bfb0b34048988eaf11a78b4073
WuZackk <jocorrales@estudiantec.cr>
2023-11-03 12:32:41 -0600

    cambio al DataBaseLocal.py y EagleDefender.db

--- logGUI/menu.py
+++ logGUI/menu.py
@@ -12,7 +12,6 @@ warnings.simplefilter(action='ignore', category=UserWarning)
 from logGUI.login import Login
 import tkinter.messagebox
 import database.DataBaseLocal as DataBase
-import game.juegoinit as juegoinit
 import database.users as users
 import re
 # customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
