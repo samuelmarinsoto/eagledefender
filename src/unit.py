5549897754cf8fe99221e1085589bf66cb86c733
Samuel Marín Soto <samarin@estudiantec.cr>
2023-11-19 22:32:23 -0600

    prueba unitaria de lanzamiento de objeto completa

--- src/unit.py
+++ src/unit.py
@@ -27,5 +27,33 @@ class testjuego(unittest.TestCase):
         self.assertEqual(pared2.tipo, 'B')
         self.assertEqual(pared2.pantalla, pantalla)
 
+    def test_poderes(self):
+        pygame.init()
+        pantalla = pygame.display.set_mode((500, 500))
         
+        bala = Bala(0, 'A', pantalla, 0, 0, 0)
+        self.assertEqual(bala.vida, 3)
+        self.assertEqual(bala.posx, 0)
+        self.assertEqual(bala.posy, 0)
+        self.assertEqual(bala.angulo, 0)
+        self.assertEqual(bala.rol, 0)
+        self.assertEqual(bala.tipo, 'A')
+        self.assertEqual(bala.pantalla, pantalla)
+
+        bala.moverse(1)
+        self.assertEqual(bala.posx, 700)
+        self.assertEqual(bala.posy, 0)
+
+        bala2 = Bala(1, 'B', pantalla, 200, 200, 90)
+        self.assertEqual(bala2.vida, 5)
+        self.assertEqual(bala2.posx, 200)
+        self.assertEqual(bala2.posy, 200)
+        self.assertEqual(bala2.angulo, 90)
+        self.assertEqual(bala2.rol, 1)
+        self.assertEqual(bala2.tipo, 'B')
+        self.assertEqual(bala2.pantalla, pantalla)
+
+        bala2.moverse(1)
+        self.assertEqual(bala2.posx, 200.00000000000006)
+        self.assertEqual(bala2.posy, 900)
         
